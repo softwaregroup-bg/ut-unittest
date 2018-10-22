5c729070d2d9d8ee7ec006bb3bff0878f25e9634
kristiyan.todorov <kristiyan.todorov@softwaregroup-bg.com>
2018-10-22 19:40:03 +0300

    fix: add environment mock for unit tests

---
+++ index.js
@@ -0,0 +1,40 @@
+const { define, get, fetch } = require('./errorApi')();
+
+module.exports = ({busConfig, errors, config, mainLib}) => {
+    const busConfigGlobal = busConfig;
+    const configGlobal = config;
+    const errorsGlobal = errors;
+    const errorApi = {
+        defineError: define,
+        getError: get,
+        fetchErrors: fetch
+    };
+
+    const init = ({busConfig, config, errors}) => Object.assign(
+        {[`_id_${Math.random()}`]: 'id'},
+        mainLib,
+        {config},
+        {
+            errors: {
+                ...errors(errorApi),
+                ...errorApi
+            },
+            bus: {
+                importMethod: (method) => (msg) => Promise.resolve(busConfig[method](msg)),
+                config: ((busConfig && busConfig.config) || {})
+            },
+            log: {error: () => {}, log: () => {}}
+        }
+    );
+    return {
+        init: ({busConfig, errors, config} = {}) => {
+            var inst = init({
+                busConfig: ((busConfig && Object.assign({}, busConfigGlobal, {config: busConfig})) || busConfigGlobal),
+                config: ((config && Object.assign({}, configGlobal, config)) || configGlobal),
+                errors: ((errors && Object.assign({}, errorsGlobal, errors)) || errorsGlobal)
+            });
+            inst.start();
+            return inst;
+        }
+    };
+};
